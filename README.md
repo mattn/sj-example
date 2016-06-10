cce265847541b59e1bb4defd1f377ef5b7e65c1d
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-06-10 10:52:27 +0900

    import

---
+++ README.md
@@ -0,0 +1,4 @@
+# sj-example
+
+![](https://raw.githubusercontent.com/mattn/sj-example/gh-pages/screenshot.gif)
+
---
+++ x-books.js
@@ -0,0 +1,48 @@
+customElements.define('x-books', class extends sj.Element {
+  template() {
+    return `
+      <h3>Books</h3>
+      <input type="text" class="books-filter" sj-model="filter" sj-keyup="keyup()" placeholder="検索するキーワードを入力して下さい" />
+      <div class="books-container">
+        <div class="books-list" sj-repeat="x in books">
+          <div class="item" sj-if="matched(x,filter)" sj-model="x.name" sj-click="clicked($index)">replace here</div>
+        </div>
+      </div>
+    `;
+  }
+
+  initialize() {
+    this.scope.filter = '';
+    this.scope.books = [];
+    this.scope.keyup = function() {
+      this.scope.filter = this.querySelector('input').value;
+      this.update();
+    };
+    this.scope.clicked = function(index) {
+      location.href = 'http://www.amazon.co.jp/gp/search/?field-keywords=' + encodeURIComponent(this.scope.books[index].name);
+    };
+    this.scope.matched = function(x,filter) {
+      return filter == '' || x.name.toLowerCase().indexOf(filter.toLowerCase()) != -1;
+    };
+  }
+
+  setBooks(books) {
+    this.scope.books = books;
+    this.update();
+  }
+});
+
+window.addEventListener("DOMContentLoaded", function(){
+  var elems = document.getElementsByTagName('x-books');
+  for (var n = 0; n < elems.length; n++) {
+    (function(elem) {
+      var ep = elem.getAttribute('endpoint') || '/query';
+      fetch(ep)
+        .then(function(response) {
+          return response.json()
+        }).then(function(json) {
+	      elem.setBooks(json);
+        });
+    })(elems[n]);
+  }
+}, false);
